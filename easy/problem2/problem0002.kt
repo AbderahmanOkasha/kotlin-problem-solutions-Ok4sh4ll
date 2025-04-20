52a9c8b4a03a99126c4bd5c064736e58e6178b7e
abderahman2651 <abderahmanmahmoud2651@gmail.com>
2025-04-20 12:28:12 +0200

    Added problem002 - reverse string

---
+++ easy/problem2/problem0002.kt
@@ -0,0 +1,15 @@
+package problem2
+
+/*
+ * Problem 001: Sum of Two Numbers
+ * Description: Write a function that returns the sum of two integers.
+ * Example: sum(3, 5) → 8
+ */
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main() {
+    println(sum(3, 5))  // Output: 8
+}
